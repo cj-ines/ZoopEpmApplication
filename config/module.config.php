368f5a9b62213f1822b6a409c3c4455a4905ecf3
Crisdel James Ines <cj.ines@outlook.ph>
2014-11-12 18:04:53 +0800

    Putting up the skeleton.

--- config/module.config.php
+++ config/module.config.php
@@ -15,7 +15,7 @@ return array(
                 'options' => array(
                     'route'    => '/',
                     'defaults' => array(
-                        'controller' => 'Application\Controller\Index',
+                        'controller' => 'ZoopEpmApplication\Controller\Index',
                         'action'     => 'index',
                     ),
                 ),
@@ -23,13 +23,13 @@ return array(
             // The following is a route to simplify getting started creating
             // new controllers and actions without needing to create a new
             // module. Simply drop new controllers in, and you can access them
-            // using the path /application/:controller/:action
-            'application' => array(
+            // using the path /zoop-epm-application/:controller/:action
+            'zoop-epm-application' => array(
                 'type'    => 'Literal',
                 'options' => array(
-                    'route'    => '/application',
+                    'route'    => '/zoop-epm-application',
                     'defaults' => array(
-                        '__NAMESPACE__' => 'Application\Controller',
+                        '__NAMESPACE__' => 'ZoopEpmApplication\Controller',
                         'controller'    => 'Index',
                         'action'        => 'index',
                     ),
@@ -73,7 +73,8 @@ return array(
     ),
     'controllers' => array(
         'invokables' => array(
-            'Application\Controller\Index' => 'Application\Controller\IndexController'
+            'ZoopEpmApplication\Controller\Index' => 'ZoopEpmApplication\Controller\IndexController',
+            'ZoopEpmApplication\Controller\Dashboard' => 'ZoopEpmApplication\Controller\DashboardController',
         ),
     ),
     'view_manager' => array(
@@ -84,7 +85,7 @@ return array(
         'exception_template'       => 'error/index',
         'template_map' => array(
             'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
-            'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
+            'zoop-epm-application/index/index' => __DIR__ . '/../view/zoop-epm-application/index/index.phtml',
             'error/404'               => __DIR__ . '/../view/error/404.phtml',
             'error/index'             => __DIR__ . '/../view/error/index.phtml',
         ),
